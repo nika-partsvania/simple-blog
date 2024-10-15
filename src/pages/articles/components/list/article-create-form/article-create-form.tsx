8e01b24e9a1d2ebedc424a332fe3ef8b65858c8d
n.partsvania <n.partsvania@livo.ge>
2024-10-15 20:13:28 +0400

    implemeneted form with controlled fields, for article creation

--- src/pages/articles/components/list/article-create-form/article-create-form.tsx
+++ src/pages/articles/components/list/article-create-form/article-create-form.tsx
@@ -1,21 +1,55 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 type ArticleCreateFormProps = {
-  onArticleCreate: (e: FormEvent<HTMLFormElement>) => void;
+  onArticleCreate: (articleFields: {
+    title: string;
+    description: string;
+  }) => void;
+  errorMsg: string;
 };
 
 const ArticleCreateForm: React.FC<ArticleCreateFormProps> = ({
   onArticleCreate,
 }) => {
+  // const [fieldErrorMsg, setFieldErrorMsg] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    if (title.length > 8) {
+      // setFieldErrorMsg("Title არ უნდა შეიცავდეს 8-ზე მეტ სიმბოლოს !");
+      // return;
+    }
+
+    setTitle(value);
+  };
+
+  const handleChangeDescription = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setDescription(value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onArticleCreate({ title, description });
+  };
+
   return (
     <form
       style={{ display: "flex", flexDirection: "column", gap: 12 }}
-      onSubmit={onArticleCreate}
+      onSubmit={handleSubmit}
     >
-      <input name="title" />
-      <input name="description" />
+      <input value={title} onChange={handleChangeTitle} name="title" />
+      <input
+        value={description}
+        onChange={handleChangeDescription}
+        name="description"
+      />
 
       <button type="submit">Create Article</button>
+      {/* <span style={{ color: "red" }}>{fieldErrorMsg}</span> */}
     </form>
   );
 };
