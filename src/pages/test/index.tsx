da3d227a3edc49ca2b46641bd5f56d304e46ccac
Nika Partsvania <n.partsvania@Nika-Partsvania.local>
2024-10-21 21:42:30 +0400

    useCallback

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,23 +1,9 @@
-// preventDefault
-// stopPropagation
+import Parent from './component/parent';
 
 const TestView = () => {
   return (
-    <div style={{ minHeight: "100vh", padding: 100 }}>
-      <div
-        onClick={() => {
-          alert("Clicked On Div");
-        }}
-      >
-        <button
-          onClick={(event) => {
-            event.stopPropagation();
-            alert("Clicked On Button");
-          }}
-        >
-          Click
-        </button>
-      </div>
+    <div style={{ minHeight: '100vh', padding: 100 }}>
+      <Parent> </Parent>
     </div>
   );
 };
