6d945e0225da155fe4f3d8c7c09d66966362bef1
Nika Partsvania <n.partsvania@Nika-Partsvania.local>
2024-10-24 14:10:19 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import DefaultLayout from "./layouts/default";
-import AboutView from "./pages/about/views/about";
-import NotFoundPage from "./pages/404";
-import { lazy, Suspense } from "react";
-import ContactInformationView from "@/pages/contact/views/contact";
-import SingleArticleView from "@/pages/articles/views/single";
-import TestView from "@/pages/test";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import DefaultLayout from './layouts/default';
+import AboutView from './pages/about/views/about';
+import NotFoundPage from './pages/404';
+import { lazy, Suspense } from 'react';
+import ContactInformationView from '@/pages/contact/views/contact';
+import SingleArticleView from '@/pages/articles/views/single';
+import TestView from '@/pages/test';
 
-const ArticlesListView = lazy(() => import("./pages/articles/views/list"));
+const ArticlesListView = lazy(() => import('./pages/articles/views/list'));
 
 function App() {
+  console.log('aba vnaxot');
   return (
     <>
       <Routes>
-        <Route path="/:lang" element={<DefaultLayout />}>
+        <Route path='/:lang' element={<DefaultLayout />}>
           <Route
-            path="articles"
+            path='articles'
             element={
               <Suspense fallback={<div>Loading...</div>}>
                 <ArticlesListView />
               </Suspense>
             }
           />
-          <Route path="articles/:id" element={<SingleArticleView />} />
-          <Route path="about" element={<AboutView />} />
-          <Route path="contact" element={<ContactInformationView />} />
-          <Route path="test" element={<TestView />} />
+          <Route path='articles/:id' element={<SingleArticleView />} />
+          <Route path='about' element={<AboutView />} />
+          <Route path='contact' element={<ContactInformationView />} />
+          <Route path='test' element={<TestView />} />
         </Route>
-        <Route path="/" element={<Navigate to="/ka/articles" />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path='/' element={<Navigate to='/ka/articles' />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </>
   );
